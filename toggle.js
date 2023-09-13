0f1683008daedf439c3b758c3bb0b725a763e17f
isha kawosa <isha@client-10-228-137-232.tamulink.tamu.edu>
2023-09-13 11:56:39 -0500

    added toggle feature to all pages

--- toggle.js
+++ toggle.js
@@ -1,10 +1,3 @@
-var num = 0; // global variable
-
-function button1() {
-    num++;
-    document.getElementById("output").innerHTML = "Press x " + num;
-}
-
 function toggleStyleSheet(){
     // Task 1
     // Steps
@@ -32,6 +25,17 @@ function toggleStyleSheet(){
     // 2 (d) For persistence when page is refreshed. save new stylesheet name to localStorage
     // hint: localStorage.setItem(name, value)
     localStorage.setItem("newstyle", newstyle)
+    // Toggle the 'toggled' class to change the button's appearance
+    var styleToggle = document.getElementById('theme-toggle');
+    styleToggle.classList.toggle('toggled');
+    // document.body.classList.toggle('dark');
+    // document.body.classList.toggle('light');
+    if (styleToggle.checked) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+
 }
 
 
@@ -50,5 +54,12 @@ window.onload = function(){
 
 
     // 2 (c) replace href attribute of html element.
-    
+    var theme = localStorage.getItem("theme");
+    var themeToggle = document.getElementById('theme-toggle');
+
+    if (theme === "dark") {
+        themeToggle.checked = true;
+    } else {
+        themeToggle.checked = false;
+    }
 }
